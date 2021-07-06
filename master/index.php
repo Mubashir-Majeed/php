e7bbe096ffb26ef44afac52b5d8b5bc586be4b16
Mubashir Majeed <mubashirmajeed16@gmail.com>
2021-07-06 04:41:01 -0700

    initial commit

---
+++ master/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//index.php
+
+include('header.php');
+
+?>
+<body>
+<div class="container">
+
+ <h1 style="text-align: center; padding-top: 150px; color: grey;">Wellcome To Online Examination System</h1>
+</div>
+</body>
